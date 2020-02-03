37d48d7e46300957917aab1fc86bea90d30b949b
Andrew Wilkes <github@urgentclick.com>
2020-02-03 11:39:47 +0900

    Added terrain scrolling capability.

--- Main.gd
+++ Main.gd
@@ -1,8 +1,15 @@
 extends Node2D
 
+var background
+var terrain
+var scroll_position = 0
+
 func _ready():
-	pass
+	background = $ParallaxBackground
+	terrain = find_node("Terrain")
+	terrain.get_parent().motion_mirroring.x = terrain.end_pos
 
 
 func _process(delta):
-	pass
+	scroll_position += delta * 500
+	background.scroll_offset.x = scroll_position
