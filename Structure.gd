72c5c95d6652b87967be796620a1ef7490b550c8
andrew-wilkes <github@urgentclick.com>
2020-02-12 16:08:38 +0900

    Add collision detection to structures

--- Structure.gd
+++ Structure.gd
@@ -2,6 +2,31 @@ extends Sprite
 
 class_name Structure
 
-export var value: int = 1
+const ACCEL = 9.8
+
+enum StructType { Building, Resource }
+enum states { STATIC, FALLING, WITH_ENEMY, WITH_PLAYER }
+
+export(StructType) var structType = StructType.Building
 export var title: String
 export var description: String
+
+var body
+var shape
+var velocity = Vector2(0, 0)
+var energy = 100
+var population = 1000
+var state = states.STATIC
+
+func _ready():
+	body = $KinematicBody2D
+	shape = $KinematicBody2D/CollisionShape2D
+
+
+func _physics_process(delta):
+	if state == states.FALLING:
+		velocity.y += delta * ACCEL
+		var collision = body.move_and_collide(velocity * delta)
+		if collision:
+			velocity.y = 0
+			state = states.STATIC
