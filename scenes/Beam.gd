d997a98af3a6a9499a3f3c954b866199d2ec6b1d
andrew-wilkes <github@urgentclick.com>
2020-03-16 19:03:15 +0900

    Add comments to beam code

--- scenes/Beam.gd
+++ scenes/Beam.gd
@@ -1,18 +1,21 @@
 extends Area2D
 
+# This implements an animated beam weapon
+
 const TEST = true
 
 var line
 var shape
 var tween
-var dt = 0.15
-var step = 0
-var reach = 200
+var dt = 0.15 # time/speed value
+var step = 0 # animation step
+var reach = 200 # max length of beam
 
 func _ready():
 	line = $Line2D
 	shape = $CollisionShape2D
 	tween = $Tween
+	# sync the widths of line and collision shape
 	line.points[1].x = reach
 	shape.shape.extents.x = reach / 2
 	play_tween()
@@ -42,10 +45,12 @@ func play_tween():
 			tween.interpolate_property(shape, "position:x", reach * 1.5, reach * 2, dt)
 			tween.interpolate_property(line, "modulate", line.modulate, Color(0,0,1), dt)
 			if TEST:
+				# repeat with randomized position and reach
 				position = Vector2(100, rand_range(100, 120))
 				reach = rand_range(200, 400)
 				step = -1
 			else:
+				# dispose of this object after the tweens complete
 				queue_free()
 				return
 	tween.start()
